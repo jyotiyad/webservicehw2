b003affce70fbd7be8924f9dac46f4341871dae3
Jyoti Yadav <yadav.jyoti1@gmail.com>
2017-02-05 00:56:57 +0100

    working login client

--- webservice-client/src/main/java/com/jyoti/ws/client/Client.java
+++ webservice-client/src/main/java/com/jyoti/ws/client/Client.java
@@ -1,58 +1,30 @@
 package com.jyoti.ws.client;
 
+import java.net.MalformedURLException;
+import java.net.URL;
+
 public class Client {
-    private static String username = null;
-    private static String password = null;
-    private static String dCity = null;
-    private static String aCity = null;
+    private static URL url = getWSDLURL("http://localhost:8080/webservice-hw2/FlightBookingService?wsdl");
 
-    /*public static void main(String[] args){
+    public static void main(String[] args) throws AuthenticationException_Exception {
 
-        System.out.println("username:");
-        Scanner input = new Scanner(System.in);
-        username = input.next();
-        System.out.println("password:");
-        password  = input.next();
+        FlightBookingServiceService service = new FlightBookingServiceService(url);
+        FlightBookingService flightBookingService = service.getFlightBookingServicePort();
+        String login = flightBookingService.login("test", "test");
+        System.out.println("login response=" + login);
 
-        AuthenticationService login  = new AuthenticationService();
-        if(login.authenticateUser(username, password)){
-            System.out.println("Login success!");
+    }
+
+    private static URL getWSDLURL(String urlStr) {
+        URL url = null;
+        try {
+            url = new URL(urlStr);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+            throw new RuntimeException(e);
         }
-        else {
-            System.out.println("invalid username or password\nExiting.");
-        }
-        System.out.println("Enter Departure City:");
-        dCity = input.next();
-        System.out.println(dCity + "->");
-        System.out.println("Enter Arrival City:");
-        aCity = input.next();
-        System.out.println(dCity + "->" + aCity + "\n");
-
-        System.out.println("Available Flights:");
-        FlightService fs = new FlightService();
-        Set<Itinerary> flights = fs.searchFlights(dCity, aCity);
-        int i = 1;
-        for(Itinerary it : flights){
-            System.out.println("Choice #" + i );
-            List<Flight> temp = it.getFlights();
-            String s = "";
-            int price = 0 ;
-            for(Flight f : temp ){
-                s += f.getDepartureCity() + "->" + f.getDestinationCity();
-                price += f.getPrice();
-            }
-           System.out.println(s);
-            System.out.println("Price: $" + price);
-        }
-        System.out.println("Enter your choice number: ");
-        int choice = input.nextInt();
-        while( choice > flights.size() || choice <= 0 ){
-            System.out.println("Invalid entry!");
-            System.out.println("Enter your choice number: ");
-            choice = input.nextInt();
-        }
-
-    }*/
-
+        return url;
+    }
 
 }
+
